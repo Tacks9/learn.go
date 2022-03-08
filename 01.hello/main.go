b46d5d7862f83bf206453c9ab1bcf38603050b83
Tacks9 <tacks321@qq.com>
2022-03-08 23:52:56 +0800

    初始变量的定义，包裹着双引号的字符串

---
+++ 01.hello/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "go语言"
+	fmt.Println(name)
+
+	var age int = 24
+	fmt.Println(age)
+
+	var tall float64 = 182.22
+	fmt.Println(tall)
+}
